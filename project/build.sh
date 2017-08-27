b47d1fa6f9a28ec29ebd0927b95e356d21e5d956
daniel <daniel@snail.org.cn>
2017-08-27 20:35:32 +0800

    更新镜像和脚步

--- project/build.sh
+++ project/build.sh
@@ -1,32 +1,56 @@
 #!/bin/bash
 [ "$(id -u)" = "0" ] || exec sudo bash $0 $@
 
-function quit() {
+function _quit() {
 	code=$1
 	shift
 	echo "$@"
 	exit $code
 }
 
+function _write() {
+	which pv > /dev/null || apt-get install -y pv
+	echo "Info: write image"
+	cat opiz-debian-jessie-3.4.113.img.gz.?? | gzip -d | pv | dd if=/dev/stdin of=$CARD bs=1M || exit 2
+	echo "Info: synchronize..."
+	sync
+}
+
+function _expand() {
+	echo "Info: resize partition"
+	echo "p:d:n:p::::w:" | tr ':' '\n' | fdisk $CARD || exit 2
+	e2fsck -f ${CARD}1
+	resize2fs ${CARD}1
+	sync
+}
+
+MESSAGE_1="Usage: $THIS <card> [mode:write|expand|all]"
+MESSAGE_2="Error: card:$CARD not is block device"
+MESSAGE_3="Errot: device is mounted"
+
 THIS=$(readlink -f "$0")
 HOME=$(dirname "$THIS")
 CARD=$1
-
+MODE=$2
 cd $HOME
 
+[ -z "$MODE" ] && MODE=all
+[ -z "$CARD" ] && _quit 1 $MESSAGE_1
+[ -b "$CARD" ] || _quit 1 $MESSAGE_2
+mount | grep $CARD && _quit 1 $MESSAGE_3
 
-which pv > /dev/null || apt-get install -y pv
-[ -z "$CARD" ] && quit 1 "Usage: $0 <card>"
-[ -b "$CARD" ] || quit 1 "Error: card:$CARD not is block device"
-mount | grep $CARD && quit 1 "Errot: device is mounted"
-
-echo "Info: write image"
-cat opiz-debian-jessie-3.4.113.img.gz.?? | gzip -d | pv | dd if=/dev/stdin of=$CARD bs=1M || exit 2
-echo "Info: synchronize..."
-sync
-
-echo "Info: resize partition"
-echo "p:d:n:p::::w:" | tr ':' '\n' | fdisk $CARD || exit 2
-e2fsck -f ${CARD}1
-resize2fs ${CARD}1
-sync
+case "$MODE" in
+	write)
+		_write
+	;;
+	expand)
+		_expand
+	;;
+	all)
+		_write
+		_expand
+	;;
+	*)
+		echo $MESSAGE_1
+	;;
+esac
